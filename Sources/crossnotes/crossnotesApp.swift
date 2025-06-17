e3d4bf573eaec778c1f292bbc4cdad744f3569f6
AloneAlexandros <98485800+AloneAlexandros@users.noreply.github.com>
2025-06-17 15:31:32 +0300

    Example view that lists all notes

--- Sources/crossnotes/crossnotesApp.swift
+++ Sources/crossnotes/crossnotesApp.swift
@@ -7,13 +7,19 @@ struct crossnotesApp: App {
     var body: some Scene {
         @State var database = Database()
         WindowGroup("crossnotes") {
-            HelloWorldView()
+            NotesTestView(database: $database)
         }
     }
 }
 
-struct HelloWorldView : View{
+struct NotesTestView: View{
+    @Binding var database: Database
     var body: some View{
-        Text("haiiiiiiii")
+        ForEach(database.notes){ note in
+            Text(note.title)
+                .font(.noteTitle)
+            Text(note.content)
+                .padding(.bottom, 20)
+        }
     }
 }
---
+++ Sources/crossnotes/customFonts.swift
@@ -0,0 +1,7 @@
+import SwiftCrossUI
+
+extension Font{
+    static var noteTitle: Font {
+        Font.system(size: 25, weight: .bold, design: .default)
+    }
+}
