54d154d59bec4d81215fc309c79d6ff76841bd98
AloneAlexandros <98485800+AloneAlexandros@users.noreply.github.com>
2025-06-18 23:52:03 +0300

    Added a view for creating new notes

--- Sources/crossnotes/crossnotesApp.swift
+++ Sources/crossnotes/crossnotesApp.swift
@@ -1,5 +1,6 @@
 import SwiftCrossUI
 import DefaultBackend
+import Foundation
 
 @main
 struct crossnotesApp: App {
@@ -7,6 +8,7 @@ struct crossnotesApp: App {
     var body: some Scene {
         WindowGroup("crossnotes") {
             NotesTestView(database: $database)
+            NoteCreationView(database: $database)
         }
     }
 }
@@ -19,6 +21,26 @@ struct NotesTestView: View{
                 .font(.noteTitle)
             Text(note.content)
                 .padding(.bottom, 20)
+        }
+    }
+}
+
+struct NoteCreationView: View{
+    @Binding var database: Database
+    @State var noteTitle = ""
+    @State var chosenFolder: URL? = nil
+    var body: some View{
+        TextField("Note title", text: $noteTitle)
+        Picker(of: database.savesDirectories, selection: $chosenFolder)
+        let nameExists = database.notes.filter{ $0.title == noteTitle}
+        if chosenFolder != nil && noteTitle != "" && nameExists.count == 0{
+            Button("Create note", action: {
+                database.createNote(title: noteTitle, folder: chosenFolder!)
+                //TODO: navigate away
+            })
+        } else if nameExists.count != 0{
+            Text("Note with the same name already exists")
+                .foregroundColor(.red)
         }
     }
 }
