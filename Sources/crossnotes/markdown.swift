bbdd89d0a377cd4d1ddbd20ba4e9f838bad5e2c6
Alexandros Samonakis <98485800+AloneAlexandros@users.noreply.github.com>
2025-08-18 11:40:14 +0300

    added support for italics & small text!

--- Sources/crossnotes/markdown.swift
+++ Sources/crossnotes/markdown.swift
@@ -29,6 +29,8 @@ struct MarkdownText: View{
         }
         else if (text.hasPrefix("###### ")){
             return Font.system(size: 19)
+        }else if (text.hasPrefix("-# ")){
+            return Font.system(size: 10)
         }else{
             return Font.system(size: 16)
         }
@@ -47,6 +49,8 @@ struct MarkdownText: View{
             return String(text.dropFirst(5))
         }else if (text.hasPrefix("##### ")){
             return String(text.dropFirst(6))
+        }else if (text.hasPrefix("-# ")){
+            return String(text.dropFirst(3))
         }
         else if (text.hasPrefix("###### ")){
             return String(text.dropFirst(7))
@@ -70,7 +74,8 @@ struct InLineMarkdowns: View{
     }
 }
 
-//it appears that if you put two consecutive bolds like **text** **text** none get bold. I don't know how to fix it, and it seems specific enough not to worry about
+
+//it appears that if you put two consecutive bolds like **text** **text** none get bold. I don't know how to fix it, and it is not expected for someone to do that anyway
 struct Bold: View{
     @State var text: String
     @State var i = 1
@@ -81,10 +86,44 @@ struct Bold: View{
             ForEach(Array(betterSplitted.enumerated())){i,string in
                 if(((betterSplitted.count-i)%2==0 || betterSplitted.count == 1) && splitted.count != 1)
                 {
-                    Text(string)
+                    Italic(text: string)
                         .emphasized()
+                }else{
+                    
+                    Italic(text: stringToShow(string: string))
+                    
+                }
+            }
+        }
+    }
+
+    func stringToShow(string:String) -> String
+    {
+        var stringToShow = string
+        if(string.first == " "){
+            stringToShow.remove(at: stringToShow.startIndex)
+        }
+        if(string.last == " ")
+        {
+            stringToShow = String(stringToShow.dropLast())
+        }
+        return stringToShow
+    }
+}
+//i assume putting two italics in a row, like *text* *text* makes none of them italic, however this is no porblem since it's not expected for a user to do that anyway
+struct Italic: View{
+    @State var text: String
+    @State var i = 1
+    var body: some View{
+        let splitted = text.components(separatedBy: "*")
+        let betterSplitted = splitted.filter{ $0 != ""}
+        HStack{
+            ForEach(Array(betterSplitted.enumerated())){i,string in
+                if(((betterSplitted.count-i)%2==0 || betterSplitted.count == 1) && splitted.count != 1)
+                {
+                    Text(string)
+                        .italic()
                         .padding(.leading, -5)
-                        //FIXME: find italics
                 }else{
                     
                     Text(stringToShow(string: string))
@@ -107,5 +146,3 @@ struct Bold: View{
         return stringToShow
     }
 }
-
-//unfortunately crossui doesn't seem to support italics
